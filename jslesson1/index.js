eab63f4b6a3e0c047c38e6cbd9ef9b591cb24f2e
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2025-04-07 19:45:22 +0300

    jslesson1

---
+++ jslesson1/index.js
@@ -0,0 +1,100 @@
+// alert("Привіт усім"); // Вітання з користувачем
+
+//тут було привітання з користувачем
+
+// alert("Привіт усім"); 
+// alert("Привіт усім"); 
+// alert("Привіт усім"); 
+
+
+// console.log("Привіт світ!");
+// console.log("5");
+
+// let const var
+
+
+// let name = "Volodymyr";
+
+// name = "Ivan";
+
+// name = 5;
+
+// const NAME = "Volodymyr";
+
+
+// name = "Ivan";
+
+// let name, age;
+
+// name = "Volodymyr";
+
+// age = 25;
+
+// console.log(name);
+// console.log(age);
+
+// console.log("My name is " + name);
+
+// console.log(`My name is ${name}`);
+
+// console.log("The " + name + " is good");
+// console.log(`The ${name} is good`);
+
+// console.log("Text1" + "Text2");
+// console.log(5 + 5);
+
+// Типи даних
+
+// String  - текстовий формат
+
+// Number 
+
+// Boolean - true - false
+
+// Undefined 
+
+// null - пусті дані
+
+// Object
+
+// `string`
+
+// 5 10 20.5 11.65353
+
+// true false
+// let name = "Volodymyr";
+
+// name = null;
+
+// const user = {
+//     name: "Volodymyr",
+//     surname: "Pankiv",
+//     city: "Lviv",
+//     online: true,
+//     age: 100,
+// };
+
+
+// Task2
+
+// console.log("Pankiv");
+
+
+// Task3
+
+// ....
+
+// Task4
+
+// ....
+
+
+
+
+
+
+
+
+
+
+
